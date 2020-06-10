a66ff7d11379d31990d05c30b099b6d5c286078d
RubyJean <976235684@qq.com>
2020-06-10 17:22:02 +0800

    机器人常量创建

---
+++ fight-landlord-view/src/main/java/org/view/coolq/constant/RobotConstant.java
@@ -0,0 +1,11 @@
+package org.view.coolq.constant;
+
+/**
+ * 机器人使用的常量
+ * @author RubyJing
+ * @version 1.0
+ * @date 2020/6/10 10:12
+ */
+public class RobotConstant {
+    public static final String START_FLAG = "斗地主启动";
+}
