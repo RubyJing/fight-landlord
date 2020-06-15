03577df0941e248266d1964fdae7fb29ad1c04b3
RubyJean <976235684@qq.com>
2020-06-15 17:24:22 +0800

    返回队列

---
+++ fight-landlord-view/src/main/java/org/view/coolq/output/OutputInfo.java
@@ -0,0 +1,22 @@
+package org.view.coolq.output;
+
+
+import org.view.coolq.entity.Response;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * 统一返回输出
+ * @author RubyJing
+ * @version 1.0
+ * @date 2020/6/15 14:48
+ */
+public class OutputInfo {
+
+    public static BlockingQueue<String> messageQueue = new LinkedBlockingQueue<>();
+
+    public static BlockingQueue<Response> privateMessageQueue = new LinkedBlockingQueue<>();
+
+
+}
