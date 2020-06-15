78bf1590d5a9953b9189b525adbfc69f0e7a45e0
RubyJean <976235684@qq.com>
2020-06-15 17:24:43 +0800

    返回信息队列Vo

---
+++ fight-landlord-view/src/main/java/org/view/coolq/entity/Response.java
@@ -0,0 +1,34 @@
+package org.view.coolq.entity;
+
+/**
+ * 返回消息
+ * @author RubyJing
+ * @version 1.0
+ * @date 2020/6/15 17:16
+ */
+public class Response {
+    private Long playerQq;
+
+    private String message;
+
+    public Long getPlayerQq() {
+        return playerQq;
+    }
+
+    public void setPlayerQq(Long playerQq) {
+        this.playerQq = playerQq;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Response(Long playerQq, String message) {
+        this.playerQq = playerQq;
+        this.message = message;
+    }
+}
