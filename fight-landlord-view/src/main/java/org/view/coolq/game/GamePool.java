c145256b97072cf5092b92a5fef4e0e6abbd902d
RubyJean <976235684@qq.com>
2020-06-16 17:30:01 +0800

    新增当前操作人缓存池

--- fight-landlord-view/src/main/java/org/view/coolq/game/GamePool.java
+++ fight-landlord-view/src/main/java/org/view/coolq/game/GamePool.java
@@ -1,6 +1,8 @@
 package org.view.coolq.game;
 
 import entity.Game;
+import entity.Player;
+import org.view.coolq.entity.GameCurrPlayer;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -18,6 +20,52 @@ public class GamePool {
      * 对局缓存池
      */
     public static List<Game> games = new ArrayList<>();
+
+    /**
+     * 对局当前操作者缓存池
+     */
+    public static List<GameCurrPlayer> currPlayers = new ArrayList<>();
+
+    /**
+     * 获取当前群当前操作玩家
+     *
+     * @param groupId 群qq
+     * @return GameCurrPlayer
+     */
+    public static Player getCurrPlayer(Long groupId) {
+        if (currPlayers != null && currPlayers.size() != 0) {
+            for (GameCurrPlayer currPlayer : currPlayers) {
+                if (currPlayer.getGroupId() == groupId) {
+                    return currPlayer.getCurrPlayer();
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 新增/修改 群对局当前操作玩家
+     *
+     * @param player  玩家
+     * @param groupId 群qq
+     */
+    public static void addOrUpdateGameCurrPlayer(Player player, Long groupId) {
+        if (currPlayers.size() == 0) {
+            currPlayers.add(new GameCurrPlayer(groupId, player));
+        } else {
+            boolean isHave = false;
+            for (GameCurrPlayer currPlayer : currPlayers) {
+                if (currPlayer.getGroupId() == groupId) {
+                    currPlayer.setCurrPlayer(player);
+                    isHave = true;
+                    break;
+                }
+            }
+            if (!isHave) {
+                currPlayers.add(new GameCurrPlayer(groupId, player));
+            }
+        }
+    }
 
     /**
      * 获取对局
