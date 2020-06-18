c22a59e0cb39a86cb6a92f67c2305abd71b98b03
RubyJean <976235684@qq.com>
2020-06-18 09:04:27 +0800

    选择地主测试

--- fight-landlord-core/src/test/java/GameTest.java
+++ fight-landlord-core/src/test/java/GameTest.java
@@ -1,3 +1,4 @@
+import card.CardFactory;
 import entity.Game;
 import entity.GameCardVo;
 import entity.Player;
@@ -15,29 +16,36 @@ import java.util.List;
  */
 public class GameTest {
     public static void main(String[] args) {
+        new CardFactory();
         RobotServiceImpl robotService = new RobotServiceImpl();
         ImageServiceImpl imageService = new ImageServiceImpl();
         List<Player> players = new ArrayList<>(3);
         for (int i = 1; i < 4; i++) {
             Player player = new Player();
-            player.setId((long) i);
+            player.setId(i);
             player.setPlayerName("玩家" + i);
             players.add(player);
+            player.setQqNum(i);
         }
         Game game = new Game();
         game.setGroupId(1L);
-        game.setCards(robotService.initCard());
+        game.setCards(CardFactory.initCardPool);
         game.setPlayers(players);
         robotService.licensing(game);
         List<Player> gamePlayers = game.getPlayers();
         for (Player gamePlayer : gamePlayers) {
             System.out.println(gamePlayer.getPlayerName() + "的牌是：");
+            imageService.addSubscript(gamePlayer.getCards());
             System.out.println(imageService.gameCardsImage(gamePlayer.getCards()));
         }
         System.out.println("===========场上剩余卡牌===========");
         for (GameCardVo card : game.getCards()) {
             System.out.print(imageService.gameCardImage(card));
         }
+        robotService.getLandLord(game,1);
+        System.out.println("\n地主的牌是：");
+        imageService.addSubscript(players.get(0).getCards());
+        System.out.println(imageService.gameCardsImage(players.get(0).getCards()));
 
 
 
