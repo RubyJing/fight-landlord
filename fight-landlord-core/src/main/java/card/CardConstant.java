04206de3d824e0366da04ae4d7734add1727bbe9
RubyJean <976235684@qq.com>
2020-06-19 10:50:14 +0800

    加入牌组常量

---
+++ fight-landlord-core/src/main/java/card/CardConstant.java
@@ -0,0 +1,19 @@
+package card;
+
+/**
+ * 牌常量
+ * @author RubyJing
+ * @version 1.0
+ * @date 2020/6/18 14:17
+ */
+public class CardConstant {
+
+    /**
+     * 键盘样式
+     */
+    public static final String[] KEY_BOARDS = new String[]{"⓵", "⓶", "⓷", "⓸", "⓹", "⓺", "⓻", "⓼", "⓽", "⓿", "Ⓠ", "Ⓦ", "Ⓔ", "Ⓡ", "Ⓣ", "Ⓨ", "Ⓤ", "Ⓘ", "Ⓞ", "Ⓟ"};
+    /**
+     * 键盘值
+     */
+    public static final String[] KEY_BOARDS_VALUE = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"};
+}
