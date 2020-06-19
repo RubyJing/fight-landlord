01f9a314b4edc148765b6358f680ddae6e1f07ec
RubyJean <976235684@qq.com>
2020-06-19 17:32:12 +0800

    打牌测试

--- fight-landlord-core/src/test/java/GameTest.java
+++ fight-landlord-core/src/test/java/GameTest.java
@@ -3,11 +3,15 @@ import entity.Game;
 import entity.GameCardVo;
 import entity.Player;
 import service.ImageServiceImpl;
+import service.PlayService;
+import service.PlayServiceImpl;
 import service.RobotServiceImpl;
 import util.Print;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author RubyJing
@@ -19,6 +23,7 @@ public class GameTest {
         new CardFactory();
         RobotServiceImpl robotService = new RobotServiceImpl();
         ImageServiceImpl imageService = new ImageServiceImpl();
+        PlayService playService = new PlayServiceImpl();
         List<Player> players = new ArrayList<>(3);
         for (int i = 1; i < 4; i++) {
             Player player = new Player();
@@ -36,17 +41,34 @@ public class GameTest {
         for (Player gamePlayer : gamePlayers) {
             System.out.println(gamePlayer.getPlayerName() + "的牌是：");
             imageService.addSubscript(gamePlayer.getCards());
-            System.out.println(imageService.gameCardsImage(gamePlayer.getCards()));
+            System.out.println(imageService.gameCardsImage(gamePlayer.getCards(), true));
         }
         System.out.println("===========场上剩余卡牌===========");
         for (GameCardVo card : game.getCards()) {
-            System.out.print(imageService.gameCardImage(card));
+            System.out.print(imageService.gameCardImage(card, false));
         }
-        robotService.getLandLord(game,1);
+        robotService.getLandLord(game, 1);
         System.out.println("\n地主的牌是：");
         imageService.addSubscript(players.get(0).getCards());
-        System.out.println(imageService.gameCardsImage(players.get(0).getCards()));
-
+        System.out.println(imageService.gameCardsImage(players.get(0).getCards(), true));
+        while (playService.getAllNotHitCards(players.get(0).getCards()).size() != 0) {
+            System.out.print("选择出牌：");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (playService.isKeyBoardsInputLegal(game,playService.getAllNotHitCards(players.get(0).getCards()), input)) {
+                Print.PN("场上" + players.get(0).getPlayerName() + "打出：",Print.GREEN);
+                Print.PN(playService.playerSendCard(game, 1, input), Print.RED, Print.REVERSE);
+                System.out.println("您的手牌:");
+                List<GameCardVo> gameCardVos = playService.getAllNotHitCards(players.get(0).getCards());
+                if (gameCardVos.size() == 0) {
+                    System.out.println("恭喜您获得胜利");
+                } else {
+                    System.out.println(imageService.sendCardImage(gameCardVos));
+                }
+            } else {
+                System.out.println("输入格式错误");
+            }
+        }
 
 
     }
