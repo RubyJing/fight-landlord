6dae96dd2af79c9d0a92d6922afd225f56afc1da
RubyJean <976235684@qq.com>
2020-06-19 17:31:48 +0800

    包装样式改成可以不输出键盘符合的

--- fight-landlord-core/src/main/java/service/ImageService.java
+++ fight-landlord-core/src/main/java/service/ImageService.java
@@ -15,20 +15,29 @@ public interface ImageService {
     /**
      * 包装牌样式
      * @param gameCardVo 牌
+     * @param subscript true 加入下标
      * @return String
      */
-    String gameCardImage(GameCardVo gameCardVo);
+    String gameCardImage(GameCardVo gameCardVo,boolean subscript);
 
     /**
      * 包装牌组 和 键盘符样式
      * @param gameCardVos 牌vos
+     * @param subscript true 加入下标
      * @return String
      */
-    String gameCardsImage(List<GameCardVo> gameCardVos);
+    String gameCardsImage(List<GameCardVo> gameCardVos,boolean subscript);
 
     /**
      * 加入键盘标识
      * @param gameCardVos 牌vos
      */
     void addSubscript(List<GameCardVo> gameCardVos);
+
+    /**
+     * 出牌（重新加入标识，加入样式）
+     * @param gameCardVos 牌vos
+     * @return 已包装好玩家当前手牌
+     */
+    String sendCardImage(List<GameCardVo> gameCardVos);
 }
--- fight-landlord-core/src/main/java/service/ImageServiceImpl.java
+++ fight-landlord-core/src/main/java/service/ImageServiceImpl.java
@@ -1,5 +1,6 @@
 package service;
 
+import card.CardConstant;
 import entity.GameCardVo;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,36 +15,44 @@ import java.util.List;
 @Slf4j
 public class ImageServiceImpl implements ImageService {
     @Override
-    public String gameCardImage(GameCardVo gameCardVo) {
+    public String gameCardImage(GameCardVo gameCardVo,boolean subscript) {
         StringBuilder cardImage = new StringBuilder("|");
         cardImage.append(gameCardVo.getCardType().getGraph());
         cardImage.append(gameCardVo.getCard().getCardName());
         cardImage.append("|");
-        if (gameCardVo.getSubscript() != null) {
-            cardImage.append(gameCardVo.getSubscript()+" ");
+        if (subscript){
+            if (gameCardVo.getSubscript() != null) {
+                cardImage.append(gameCardVo.getSubscript()).append(" ");
+            }
         }
         return cardImage.toString();
     }
 
 
     @Override
-    public String gameCardsImage(List<GameCardVo> gameCardVos) {
+    public String gameCardsImage(List<GameCardVo> gameCardVos,boolean subscript) {
         StringBuilder cardImages = new StringBuilder();
         for (GameCardVo gameCardVo : gameCardVos) {
-            cardImages.append(this.gameCardImage(gameCardVo));
+            cardImages.append(this.gameCardImage(gameCardVo,subscript));
         }
         return cardImages.toString();
     }
 
 
+
     @Override
     public void addSubscript(List<GameCardVo> gameCardVos) {
-        String[] keyBoards = new String[]{"⓵", "⓶", "⓷", "⓸", "⓹", "⓺", "⓻", "⓼", "⓽", "⓿", "Ⓠ", "Ⓦ", "Ⓔ", "Ⓡ", "Ⓣ", "Ⓨ", "Ⓤ", "Ⓘ", "Ⓞ", "Ⓟ"};
         for (int i = 0; i < gameCardVos.size(); i++) {
-            gameCardVos.get(i).setSubscript(keyBoards[i]);
+            gameCardVos.get(i).setSubscript(CardConstant.KEY_BOARDS[i]);
+            gameCardVos.get(i).setSubscriptValue(CardConstant.KEY_BOARDS_VALUE[i]);
         }
     }
 
+    @Override
+    public String sendCardImage(List<GameCardVo> gameCardVos) {
+        this.addSubscript(gameCardVos);
+        return this.gameCardsImage(gameCardVos,true);
+    }
 
 
 }
