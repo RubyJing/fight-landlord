2c4a3bdb0fceb578de6cd64eb615aef45805cf2a
RubyJean <976235684@qq.com>
2020-06-28 15:08:13 +0800

    加入判断项，对局不能有重复的人参加

--- fight-landlord-view/src/main/java/org/view/coolq/listener/CoolGroupListener.java
+++ fight-landlord-view/src/main/java/org/view/coolq/listener/CoolGroupListener.java
@@ -115,23 +115,33 @@ public class CoolGroupListener extends IcqListener {
                     for (Game game : games) {
                         if (game.getGroupId().equals(eventGroupMessage.getGroupId())) {
                             List<Player> players = game.getPlayers();
-                            Player player = new Player();
-                            player.setQqNum(eventGroupMessage.getSender().getId());
-                            player.setPlayerName(eventGroupMessage.getSender().getInfo().getNickname());
-                            players.add(player);
-                            if (players.size() == 3) {
-                                try {
-                                    game.getGroupQueue().put("licensing");
-                                    eventGroupMessage.respond("人数已满，对局开始");
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
+                            //判断用户是否已加入
+                            boolean isHavePlayer = false;
+                            for (Player player : players) {
+                                if (player.getQqNum() == eventGroupMessage.getSender().getId()) {
+                                    isHavePlayer = true;
+                                    break;
                                 }
-                                game.setIsStart(true);
-                                GamePool.addGame(game);
                             }
-                            if (players.size() > 3) {
-                                eventGroupMessage.respond("抱歉，游戏人数已满，无法加入对局");
-                                return;
+                            if (!isHavePlayer) {
+                                Player player = new Player();
+                                player.setQqNum(eventGroupMessage.getSender().getId());
+                                player.setPlayerName(eventGroupMessage.getSender().getInfo().getNickname());
+                                players.add(player);
+                                if (players.size() == 3) {
+                                    try {
+                                        game.getGroupQueue().put("licensing");
+                                        eventGroupMessage.respond("人数已满，对局开始");
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                    game.setIsStart(true);
+                                    GamePool.addGame(game);
+                                }
+                                if (players.size() > 3) {
+                                    eventGroupMessage.respond("抱歉，游戏人数已满，无法加入对局");
+                                    return;
+                                }
                             }
                         }
                     }
